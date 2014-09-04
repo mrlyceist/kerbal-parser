ad3126c41cf3ffba3cf82bb52086d7b59634fe8c
Jakob Calero <jakob.calero@gmail.com>
2014-09-04 22:04:01 +0100

    Add basic skeleton for parser and KerbalNode class

--- KerbalParser/KerbalParser.cs
+++ KerbalParser/KerbalParser.cs
@@ -1,4 +1,35 @@
-﻿namespace KerbalParser
+using System;
+using System.Collections.Generic;
+
+namespace KerbalParser
 {
-	public class KerbalParser {}
+	public class KerbalParser
+	{
+		public static IList<KerbalNode> ParseConfig(String configFile)
+		{
+			IList<KerbalNode> kerbalNodes = new List<KerbalNode>();
+
+			return kerbalNodes;
+		}
+	}
+
+	public class KerbalNode
+	{
+		public KerbalNode(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get; set; }
+
+		public IDictionary<string, string> Values
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public IList<KerbalNode> Children
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
 }
