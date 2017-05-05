ac65ac009969c121b312f2d51d5528dfd0d94705
dorogov <dorogov@skbkontur.ru>
2017-05-05 17:57:53 +0500

    added first concious test

--- KerbalParserTests/FullFolderTest.cs
+++ KerbalParserTests/FullFolderTest.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace KerbalParserTests
 {
@@ -73,104 +72,104 @@ namespace KerbalParserTests
         //    }
         //}
 
-        [Test]
-        public void TestRandomValues()
-        {
-            var allFileNames = _configs.Select(config => config.FileName)
-                                       .ToList();
+        //[Test]
+        //public void TestRandomValues()
+        //{
+        //    var allFileNames = _configs.Select(config => config.FileName)
+        //                               .ToList();
 
-            string[] filesToTest =
-            {
-                BaseDir + "\\Squad\\Resources\\ResourcesGeneric.cfg",
-                //BaseDir + "\\NASAmission\\Parts\\LaunchEscapeSystem\\part.cfg",
-                BaseDir + "\\Squad\\Parts\\Wheel\\roverWheel1\\part.cfg",
-                BaseDir + "\\Squad\\Props\\throttle\\prop.cfg"
-            };
+        //    string[] filesToTest =
+        //    {
+        //        BaseDir + "\\Squad\\Resources\\ResourcesGeneric.cfg",
+        //        //BaseDir + "\\NASAmission\\Parts\\LaunchEscapeSystem\\part.cfg",
+        //        BaseDir + "\\Squad\\Parts\\Wheel\\roverWheel1\\part.cfg",
+        //        BaseDir + "\\Squad\\Props\\throttle\\prop.cfg"
+        //    };
 
-            foreach (var file in filesToTest)
-            {
-                Assert.Contains(file, allFileNames);
-            }
+        //    foreach (var file in filesToTest)
+        //    {
+        //        Assert.Contains(file, allFileNames);
+        //    }
 
-            var configsToTest = _configs.Where
-                (config => filesToTest.Contains(config.FileName)).ToList();
+        //    var configsToTest = _configs.Where
+        //        (config => filesToTest.Contains(config.FileName)).ToList();
 
-            Assert.AreEqual(filesToTest.Length, configsToTest.Count);
+        //    Assert.AreEqual(filesToTest.Length, configsToTest.Count);
 
-            foreach (var config in configsToTest)
-            {
-                switch (config.FileName)
-                {
-                    case BaseDir +
-                         "\\Squad\\Resources\\ResourcesGeneric.cfg":
-                        Assert.
-                            IsTrue(config.First().Values.ContainsKey("name"));
-                        Assert.
-                            AreEqual(
-                                     "LiquidFuel",
-                                     config.First().Values["name"].First());
-                        break;
-                    case BaseDir +
-                         "\\NASAmission\\Parts\\LaunchEscapeSystem\\part.cfg":
-                        Assert.
-                            Greater(config.First().Children.Count, 0);
+        //    foreach (var config in configsToTest)
+        //    {
+        //        switch (config.FileName)
+        //        {
+        //            case BaseDir +
+        //                 "\\Squad\\Resources\\ResourcesGeneric.cfg":
+        //                Assert.
+        //                    IsTrue(config.First().Values.ContainsKey("name"));
+        //                Assert.
+        //                    AreEqual(
+        //                             "LiquidFuel",
+        //                             config.First().Values["name"].First());
+        //                break;
+        //            case BaseDir +
+        //                 "\\NASAmission\\Parts\\LaunchEscapeSystem\\part.cfg":
+        //                Assert.
+        //                    Greater(config.First().Children.Count, 0);
 
-                        var child = config.First().Children.First();
-                        Assert.
-                            Greater(child.Children.Count, 0);
+        //                var child = config.First().Children.First();
+        //                Assert.
+        //                    Greater(child.Children.Count, 0);
 
-                        var subchild = child.Children.First();
-                        Assert.
-                            Greater(subchild.Children.Count, 0);
+        //                var subchild = child.Children.First();
+        //                Assert.
+        //                    Greater(subchild.Children.Count, 0);
 
-                        var subsubchild = subchild.Children.First();
-                        Console.WriteLine(
-                                          "\\Squad\\Parts\\Wheel\\" +
-                                          "roverWheel1\\part.cfg");
-                        Console.WriteLine(subsubchild);
-                        Console.WriteLine("-------------------");
-                        Assert.
-                            IsTrue(subsubchild.Values.ContainsKey("clip"));
-                        Assert.
-                            AreEqual(
-                                     "sound_rocket_spurts",
-                                     subsubchild.Values["clip"].First());
-                        break;
-                    case BaseDir +
-                         "\\Squad\\Parts\\Wheel\\roverWheel1\\part.cfg":
-                        Assert.
-                            IsTrue(config.First().Values.ContainsKey("cost"));
-                        Assert.
-                            AreEqual(
-                                     "450",
-                                     config.First().Values["cost"].First());
-                        Assert.
-                            AreEqual(
-                                     3,
-                                     config.First().Children.Count);
-                        break;
-                    case BaseDir +
-                         "\\Squad\\Props\\throttle\\prop.cfg":
+        //                var subsubchild = subchild.Children.First();
+        //                Console.WriteLine(
+        //                                  "\\Squad\\Parts\\Wheel\\" +
+        //                                  "roverWheel1\\part.cfg");
+        //                Console.WriteLine(subsubchild);
+        //                Console.WriteLine("-------------------");
+        //                Assert.
+        //                    IsTrue(subsubchild.Values.ContainsKey("clip"));
+        //                Assert.
+        //                    AreEqual(
+        //                             "sound_rocket_spurts",
+        //                             subsubchild.Values["clip"].First());
+        //                break;
+        //            case BaseDir +
+        //                 "\\Squad\\Parts\\Wheel\\roverWheel1\\part.cfg":
+        //                Assert.
+        //                    IsTrue(config.First().Values.ContainsKey("cost"));
+        //                Assert.
+        //                    AreEqual(
+        //                             "450",
+        //                             config.First().Values["cost"].First());
+        //                Assert.
+        //                    AreEqual(
+        //                             3,
+        //                             config.First().Children.Count);
+        //                break;
+        //            case BaseDir +
+        //                 "\\Squad\\Props\\throttle\\prop.cfg":
 
-                        Console.WriteLine(
-                                          "\\Squad\\Parts\\Wheel\\" +
-                                          "roverWheel1\\part.cfg");
-                        foreach (var tree in config)
-                        {
-                            Console.WriteLine(tree);
-                        }
-                        Console.WriteLine("-------------------");
+        //                Console.WriteLine(
+        //                                  "\\Squad\\Parts\\Wheel\\" +
+        //                                  "roverWheel1\\part.cfg");
+        //                foreach (var tree in config)
+        //                {
+        //                    Console.WriteLine(tree);
+        //                }
+        //                Console.WriteLine("-------------------");
 
-                        Assert.AreEqual("PROP", config.First().Name);
-                        Assert.
-                            IsTrue(config.First().Values.ContainsKey("proxy"));
-                        Assert.
-                            AreEqual(
-                                     "0, 0, 0, 	0.0225, 0.0075, 0.0225, 	0, 1, 0",
-                                     config.First().Values["proxy"].First());
-                        break;
-                }
-            }
-        }
+        //                Assert.AreEqual("PROP", config.First().Name);
+        //                Assert.
+        //                    IsTrue(config.First().Values.ContainsKey("proxy"));
+        //                Assert.
+        //                    AreEqual(
+        //                             "0, 0, 0, 	0.0225, 0.0075, 0.0225, 	0, 1, 0",
+        //                             config.First().Values["proxy"].First());
+        //                break;
+        //        }
+        //    }
+        //}
     }
 }
---
+++ KerbalParserTests/IntegrationTests.cs
@@ -0,0 +1,21 @@
+﻿using KerbalParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace KerbalParserTests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public void CapsuleTest()
+        {
+            var testPart = "..\\..\\testdata\\capsule.cfg";
+            var parser = new Parser(false, true);
+
+            var cfg = parser.ParseConfig(testPart);
+
+            Assert.IsInstanceOfType(cfg, typeof(IEnumerable<KerbalConfig>), $"Parsed {cfg.GetType()}");
+        }
+    }
+}
